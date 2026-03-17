875df6631eaeb3f6f4e14d85138bde45b1511b8d
layode-ch <layode.ch@gmail.com>
2026-03-17 10:34:41 +0100

    Add none size and Update BorderRound to also use int

--- src/Decorators/BorderRound.php
+++ src/Decorators/BorderRound.php
@@ -5,9 +5,13 @@ use DecoratorPHP\Components\IComponent;
 use DecoratorPHP\Enums\Sizes;
 
 class BorderRound extends ComponentStyle {
-    public function __construct(IComponent $component, Sizes|string $radius = Sizes::EXTRA_SMALL) {
-        if (gettype($radius) == "string")
-            $radius = Sizes::from($radius);
+    public function __construct(IComponent $component, Sizes|string|int $radius = Sizes::EXTRA_SMALL) {
+        if (!($radius instanceof Sizes)){
+            if (gettype($radius) === "int")
+                $radius = Sizes::cases()[$radius];
+            else
+                $radius = Sizes::from($radius);
+        }
         parent::__construct($component, ["rounded-$radius->value"]);
     }
 }
