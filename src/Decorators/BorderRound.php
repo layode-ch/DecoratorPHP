1b8c435460b4fffb1ee588cfd7bb464d8984d2cb
layode-ch <layode.ch@gmail.com>
2026-03-17 09:32:10 +0100

    Update BorderRound.php to allow string param

--- src/Decorators/BorderRound.php
+++ src/Decorators/BorderRound.php
@@ -5,7 +5,9 @@ use DecoratorPHP\Components\IComponent;
 use DecoratorPHP\Enums\Sizes;
 
 class BorderRound extends ComponentStyle {
-    public function __construct(IComponent $component, Sizes $radius = Sizes::EXTRA_SMALL) {
+    public function __construct(IComponent $component, Sizes|string $radius = Sizes::EXTRA_SMALL) {
+        if (gettype($radius) == "string")
+            $radius = Sizes::from($radius);
         parent::__construct($component, ["rounded-$radius->value"]);
     }
 }
