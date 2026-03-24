5b0628b501c13ffd32dfd437a5358a1734fd73f1
layode-ch <layode.ch@gmail.com>
2026-03-24 09:07:14 +0100

    Fix theme into int

--- src/Enums/Theme.php
+++ src/Enums/Theme.php
@@ -2,7 +2,7 @@
 namespace DecoratorPHP\Enums;
 
 class Theme {
-    static string $BORDER_RADIUS = "sm";
+    static int $BORDER_RADIUS = 1;
     static string $BASE_BACKGROUND_COLOR = "gray-300";
     static string $INPUT_BACKGROUND_COLOR = "red-500";
     static int $INPUT_BACKGROUND_INTENSITY = 500;
