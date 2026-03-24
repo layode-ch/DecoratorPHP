18a90ffba8554e4bdccc4420d7b8127eff237226
Daniel <bejdan65@gmail.com>
2026-03-24 10:46:05 +0100

    custom panel

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@ use DecoratorPHP\Components\Card;
 use DecoratorPHP\Components\Button;
 use DecoratorPHP\Components\Component;
 use DecoratorPHP\Decorators\AccentColor;
-use DecoratorPHP\Decorators\BorderRound;
 use DecoratorPHP\Decorators\Box;
 use DecoratorPHP\Decorators\ComponentStyle;
 use DecoratorPHP\Decorators\Cursor;
@@ -14,20 +13,25 @@ use DecoratorPHP\Decorators\RedBackground;
 use DecoratorPHP\Enums\Sizes;
 use DecoratorPHP\Enums\Theme;
 
-Theme::$BORDER_RADIUS = (int)$_GET["border-radius"] ?? 1;
-Theme::$BASE_BACKGROUND_COLOR = $_GET["base-bg"] ?? "neutral-200";
+Theme::$BASE_BORDER_RADIUS      = isset($_GET["base-border-radius"])    ? (int)$_GET["base-border-radius"] : 1;
+Theme::$BASE_BORDER_SIZE        = isset($_GET["base-border-size"])      ? (int)$_GET["base-border-size"] : 1;
+
+Theme::$INPUT_BORDER_RADIUS     = isset($_GET["input-border-radius"])   ? (int)$_GET["input-border-radius"] : 1;
+Theme::$INPUT_BORDER_SIZE       = isset($_GET["input-border-size"])     ? (int)$_GET["input-border-size"] : 1;
+
+Theme::$BASE_BACKGROUND_COLOR   = $_GET["base-bg"] ?? "neutral-200";
 Theme::$ACCENT_BACKGROUND_COLOR = $_GET["accent-bg"] ?? "neutral-400";
 
-$component = new Button("Bonjouuur");
+/*
 $component = new Cursor($component);
-$component = new BorderRound($component, Sizes::QUADRUPLE_EXTRA_LARGE);
 $component = new AccentColor($component);
 $component = new Box($component, 10, 30);
+*/
 
 $card = new Card(3,1);
 $card2 = new Card(4,1);
 
-$card->textContent = $component;
+// $card->textContent = $component;
 $card2->textContent = "Card Text";
 
 ?>
@@ -43,12 +47,11 @@ $card2->textContent = "Card Text";
 <body>
     <div class="flex w-[100vw] h-[100vh]">
         <div class="w-1/5 h-full bg-neutral-50">
-            
             <form class="p-4 gap-4 flex flex-col">
+                <h1 class="text-4xl font-semibold">Decorator 🐘</h1>
 
-                <label for="font-size">Font size</label>
-
-                <select name="font-size">
+                <label for="font-size" class="text-lg">Font size</label>
+                <select name="font-size" class="p-2 bg-neutral-200 rounded-sm" >
                     <option value="xs">Extra small</option>
                     <option value="sm">Small</option>
                     <option value="md">Medium</option>
@@ -59,27 +62,30 @@ $card2->textContent = "Card Text";
                     <option value="4xl">4XL</option>
                 </select>
 
-                <label for="base-border-radius" >Base border radius</label>
-                <input name="base-border-radius" type="range" min="0" max="8" value="<?= Theme::$BORDER_RADIUS ?>">
+                <label for="base-border-radius" class="text-lg" >Base border radius</label>
+                <input name="base-border-radius" type="range" min="0" max="8" value="<?= Theme::$BASE_BORDER_RADIUS ?>">
 
-                <label for="input-border-radius" >Base border width</label>
-                <input name="input-border-radius" type="range" min="0" max="4" value="<?= Theme::$BORDER_WIDTH ?>">
+                <label for="base-border-size" class="text-lg" >Base border size</label>
+                <input name="base-border-size" type="range" min="0" max="8" value="<?= Theme::$BASE_BORDER_SIZE ?>">
+
+                <label for="input-border-radius" class="text-lg" >Input border radius</label>
+                <input name="input-border-radius" type="range" min="0" max="4" value="<?= Theme::$INPUT_BORDER_RADIUS ?>">
 
                 <label for="base-bg">Base color</label>
                 <ul class="flex gap-4">
-                    <input type="radio" name="base-bg" value="neutral-200" class="appearance-none w-10 h-10 bg-neutral-200" <?= Theme::$BASE_BACKGROUND_COLOR == "neutral-200" ? "checked" : "" ?>>
-                    <input type="radio" name="base-bg" value="neutral-400" class="appearance-none w-10 h-10 bg-neutral-400" <?= Theme::$BASE_BACKGROUND_COLOR == "neutral-400" ? "checked" : "" ?>>
-                    <input type="radio" name="base-bg" value="neutral-600" class="appearance-none w-10 h-10 bg-neutral-600" <?= Theme::$BASE_BACKGROUND_COLOR == "neutral-600" ? "checked" : "" ?>>
+                    <input type="radio" name="base-bg" value="neutral-200" class="appearance-none w-10 h-10 bg-neutral-200 rounded-sm" <?= Theme::$BASE_BACKGROUND_COLOR == "neutral-200" ? "checked" : "" ?>>
+                    <input type="radio" name="base-bg" value="neutral-400" class="appearance-none w-10 h-10 bg-neutral-400 rounded-sm" <?= Theme::$BASE_BACKGROUND_COLOR == "neutral-400" ? "checked" : "" ?>>
+                    <input type="radio" name="base-bg" value="neutral-600" class="appearance-none w-10 h-10 bg-neutral-600 rounded-sm" <?= Theme::$BASE_BACKGROUND_COLOR == "neutral-600" ? "checked" : "" ?>>
                 </ul>   
 
                 <label for="accent-bg">Accent color</label>
                 <ul class="flex gap-4">
-                    <input type="radio" name="accent-bg" value="blue-300"   class="appearance-none w-10 h-10 bg-blue-300"  <?= Theme::$ACCENT_BACKGROUND_COLOR == "blue-300" ? "checked" : "" ?>>
-                    <input type="radio" name="accent-bg" value="red-300"    class="appearance-none w-10 h-10 bg-red-300"   <?= Theme::$ACCENT_BACKGROUND_COLOR == "red-300" ? "checked" : "" ?>>
-                    <input type="radio" name="accent-bg" value="green-300"  class="appearance-none w-10 h-10 bg-green-300" <?= Theme::$ACCENT_BACKGROUND_COLOR == "green-300" ? "checked" : "" ?>>
+                    <input type="radio" name="accent-bg" value="blue-300"   class="appearance-none w-10 h-10 bg-blue-300 rounded-sm"  <?= Theme::$ACCENT_BACKGROUND_COLOR == "blue-300" ? "checked" : "" ?>>
+                    <input type="radio" name="accent-bg" value="red-300"    class="appearance-none w-10 h-10 bg-red-300 rounded-sm"   <?= Theme::$ACCENT_BACKGROUND_COLOR == "red-300" ? "checked" : "" ?>>
+                    <input type="radio" name="accent-bg" value="green-300"  class="appearance-none w-10 h-10 bg-green-300 rounded-sm" <?= Theme::$ACCENT_BACKGROUND_COLOR == "green-300" ? "checked" : "" ?>>
                 </ul>
 
-                <button type="submit" class="bg-gray-200 p-3">Generate</button>
+                <button type="submit" class="bg-gray-200 p-3 rounded-sm">Generate</button>
             </form>
         </div>
         <div class="w-4/5 h-full bg-neutral-100 grid p-2
--- src/Enums/Theme.php
+++ src/Enums/Theme.php
@@ -2,15 +2,18 @@
 namespace DecoratorPHP\Enums;
 
 class Theme {
-    static int      $BORDER_RADIUS              = 1;
-    static int      $BORDER_WIDTH              = 1;
     static string   $ACCENT_BACKGROUND_COLOR    = "blue-600";
     static string   $ACCENT_FOREGROUND_COLOR    = "white-600";
     static string   $BASE_BACKGROUND_COLOR; 
     static string   $BASE_FOREGROUND_COLOR      = "white-300";
     static string   $INPUT_BACKGROUND_COLOR     = "red-500";
     static int      $INPUT_BACKGROUND_INTENSITY = 500;
-    static int $BASE_BORDER_SIZE = 2;
-    static string $BASE_BORDER_COLOR = "blue-500";
-    static int $BASE_BORDER_RADIUS = 1;
+
+    static int      $INPUT_BORDER_SIZE = 2;
+    static int      $INPUT_BORDER_RADIUS = 2;
+    static string   $INPUT_BORDER_COLOR = "blue-500";
+
+    static int      $BASE_BORDER_SIZE = 2;
+    static int      $BASE_BORDER_RADIUS = 1;
+    static string   $BASE_BORDER_COLOR = "neutral-600";
 }
