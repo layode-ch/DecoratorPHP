01f18a9bfeacb1d366cb5e3b53df8545af7a0221
layode-ch <layode.ch@gmail.com>
2026-03-31 11:18:09 +0200

    Update Alert

--- src/Components/Alert.php
+++ src/Components/Alert.php
@@ -2,19 +2,12 @@
 
 namespace DecoratorPHP\Components;
 
-use DecoratorPHP\Decorators\BackgroundColor;
 use DecoratorPHP\Decorators\BaseBorder;
-use DecoratorPHP\Decorators\Border;
-use DecoratorPHP\Decorators\BorderRound;
 use DecoratorPHP\Decorators\Color;
 use DecoratorPHP\Decorators\ComponentStyle;
 use DecoratorPHP\Enums\AlertType;
-use DecoratorPHP\Enums\Sizes;
-use DecoratorPHP\Enums\Theme;
 
-class Alert extends Component
-{
-
+class Alert extends Component {
     public AlertType $type;
     public string $message;
 
@@ -23,15 +16,14 @@ class Alert extends Component
         parent::__construct();
         $this->type = $type;
         $this->message = $message;
-        $component = new BaseBorder($this);
+        $component = new BaseBorder($this); 
         $component = new Color($component, $type->value. '-200', "black");
         $component = new ComponentStyle($component, ["h-fit", "w-full", "p-2", "flex", "justify-between", "mb-2"]);
         $this->classList = array_merge($this->classList, $component->classList);
     }
 
-    public function toHTML(): string
-    {
-        $js = 'console.log(this.parentElement.remove())';
+    public function toHTML(): string {
+        $js = 'this.parentElement.remove()';
         $classes = implode(' ', $this->classList);
         return "
             <div class='$classes'>
